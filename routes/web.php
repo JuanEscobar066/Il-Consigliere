4c5d7d7866ea262088cb537e7c727c8902830f2b
Jorge Barquero Villalobos <jorgebv02@gmail.com>
2019-08-20 20:53:05 -0600

    Modificación de archivos
    1. Hay varios archivos que no se necesitan.
    2. Estoy documentando zonas muy oscuras.

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//Route::get('/AnnadirUsuario','UserController@view page'); 
+//Route::get('/AnnadirUsuario','UserController@view page');
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
 
 Route::get('/', function () {
 // Redirect::to('/miembro/show');
